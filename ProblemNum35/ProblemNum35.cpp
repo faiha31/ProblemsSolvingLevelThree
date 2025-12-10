2138d0e4602e4dd1f5a2fbce39db0ad2b9da1781
Faihaa Alhadeedi <162702909+faiha31@users.noreply.github.com>
2025-12-10 15:16:15 +0300

    Add files via upload

---
+++ ProblemNum35/ProblemNum35.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Write a program to read a string then print each word in that string.*/
+
+string ReadString()
+{
+    string S;
+    cout << "Please enter your string ?\n";
+    getline(cin, S);
+    return S;
+}
+void PrintEachWordInString(string S)
+{
+    string delim = " ";//delimiter
+
+    cout << "First words of this string : \n\n";
+
+    short pos = 0;//position
+    string sWord;
+
+    while ((pos = S.find(delim)) != std::string::npos)
+    {
+        sWord = S.substr(0, pos);
+        if (sWord != "")
+            cout << sWord << endl;
+
+        S.erase(0, pos + delim.length());
+    }
+
+    if (S != "")
+        cout << S << endl;
+
+}
+int main()
+{
+
+    PrintEachWordInString(ReadString());
+
+    return 0;
+}
